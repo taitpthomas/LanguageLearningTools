b9fd8c80a442b2aeec45a700eb4d3193421e9330
Thomas Tai <thomastai@renaelectronics.com>
2020-12-28 13:22:00 -0500

    workaround playsound by os.remove('output.mp3')

--- Src/main.py
+++ Src/main.py
@@ -12,56 +12,59 @@ from google.cloud import texttospeech
 from playsound import playsound
 import pyperclip
 import keyboard
+import os
 
-# hotkey
+# Global exit flag
 done = False
 
-
-def hotkey_callback(arg1, arg2):
+def hotkey_done():
     global done
-    print("Hello from a function")
     done = True
 
 
-# add hotkey
-keyboard.add_hotkey('ctrl+shift+/', hotkey_callback, args=('triggered', 'hotkey'))
-print("waiting or hotkey")
-while not done:
-    pass
+def hotkey_speak(speak_client, speak_voice, speak_audio_config):
+    # speak the data from the clipboard
+    mytext = pyperclip.paste()
+    if len(mytext) < 500:
+        print("Info")
 
-# clipboard
-# pyperclip.copy("something")
-mytext = pyperclip.paste()
-print(mytext)
+    # synthesize_speech
+    response = speak_client.synthesize_speech(
+        input=texttospeech.SynthesisInput(text=mytext),
+        voice=speak_voice,
+        audio_config=speak_audio_config
+    )
 
-# Instantiates a client
+    # write the response's audio_content binary data to a file
+    with open("output.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+    # playsound
+    playsound('output.mp3')
+    # workaround for playsound not releasing the audio file
+    os.remove('output.mp3')
+
+
+# Instantiates a Text to speech client, create the voice and audio_config
 client = texttospeech.TextToSpeechClient()
-
-# Set the text input to be synthesized
-synthesis_input = texttospeech.SynthesisInput(text=mytext)
-
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
 # noinspection PyTypeChecker
 voice = texttospeech.VoiceSelectionParams(
     language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
 )
-
-# Select the type of audio file you want returned
+# noinspection PyTypeChecker
 audio_config = texttospeech.AudioConfig(
     audio_encoding=texttospeech.AudioEncoding.MP3
 )
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
-)
+# add hotkey
+keyboard.add_hotkey('ctrl+shift+.', hotkey_done)
+keyboard.add_hotkey('ctrl+shift+/', hotkey_speak, args=(client, voice, audio_config))
 
-# The response's audio_content is binary.
-with open("output.mp3", "wb") as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
-
-playsound('output.mp3')
+# wait loop
+print("waiting for hotkeys:")
+print("    ctrl shift / to talk")
+print("    ctrl shift . to exit")
+while not done:
+    pass
